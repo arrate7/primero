58fc3470923672d53b3462532bfd6ef5e0575d0a
arrate7 <43437355+arrate7@users.noreply.github.com>
2018-10-01 15:28:46 +0200

    ,

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduzca número");
+            int num1,num2;
+            num1 =  Convert.ToInt32(Console.ReadLine()) ;
+            Console.WriteLine("Introduzca otro número");
+            num2 = Int32.Parse(Console.ReadLine());
+           // double resultado = num1 / num2;
+            Console.WriteLine("La división " + num1 + " entre " + num2 + " es: " + num1/num2);
+            Console.ReadLine();
+
+        }
+    }
+}
